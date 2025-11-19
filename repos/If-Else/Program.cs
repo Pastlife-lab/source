54b14e3819c29d591c0eb6c3a71e0860fc0a1f49
Pastlife-lab <omikron36@hotmail.com>
2025-11-19 19:39:20 +0100

    a

---
+++ repos/If-Else/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var app = new IfElse();
+        app.Run();
+    }
+}
---
+++ repos/If-else oppgave 1/If-else oppgave 1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace If_else_oppgave_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int liteTall = 2;
+            int størreTall = 6;
+
+            
+            if (liteTall < størreTall) // her er betingelsen at liteTall skal være mindre enn størreTall
+            {
+                Console.WriteLine($"Tallet {liteTall} er mindre enn eller lik {størreTall}.");
+            }
+            else if (liteTall == størreTall)        // byttet ut liteTall manuelt for og kontrollere at dette virker
+            {
+                Console.WriteLine($"Tallet {liteTall} er lik {størreTall}.");
+            }
+            else
+            {
+                Console.WriteLine($"Tallet {liteTall} er større enn {størreTall}."); // evt enklere og lagd input for brukeren for og se forskjellen 
+
+            } 
+        }
+    }
+}
