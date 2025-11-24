062898324e3094eb7e7faf3e1986a758ade7eaf4
Pastlife-lab <omikron36@hotmail.com>
2025-11-24 09:44:04 +0100

    a

---
+++ repos/If-else oppgave 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace If_else_oppgave_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 17;
+            int number2 = 13;
+ 
+            int sum = number1 + number2;
+
+            if (sum == 30)
+            {
+                Console.WriteLine("Det var riktig");
+            }
+            else if (sum > 30 )
+            {
+                Console.WriteLine("Det ble for høyt");
+            }
+            else
+            {
+                Console.WriteLine("Det ble for lavt");
+            }
+        }   
+    }
+}
