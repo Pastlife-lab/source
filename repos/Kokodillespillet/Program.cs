9312e186a1479f6caf032146c57d183b98bebb71
Pastlife-lab <omikron36@hotmail.com>
2025-11-25 14:38:02 +0100

    a

---
+++ repos/Kokodillespillet/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Kokodillespillet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "0";
+            string input = Console.ReadLine();
+            Console.WriteLine(number);
+        }
+    }
+}
