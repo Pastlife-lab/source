3580a3f3f31c1385ffde6d0af99a703989bf2fc8
Pastlife-lab <omikron36@hotmail.com>
2025-11-25 14:33:02 +0100

    a
    while løkke og lagt til noen lærdoms kommentarer
    jeg må bli finkere til og kommentere mere i koden

---
+++ repos/while løkke/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace while_løkke
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                for (int i = 1; i <= 10; i++)   // sattt "i = 1;" for jeg vil skippe 0 og få 1 til 10 "riktig" ellers ville den ha startet på 0 og gått til 10 hvis jeg ikke har satt på <"="
+                {
+                    Console.WriteLine("Runde nummer: " + i);
+                }
+                break;   //satt på en brems så den ikke bare kjører avgårde i evig løkke landevei =)
+            }
+        }
+    }
+}
+//                                          Oppgave 3:
+//                  Lag en while-løkke som printer ut "Runde nummer:" + et tall som øker med 1 per runde, så lenge
+//                  rundetelleren er mindre enn 10
