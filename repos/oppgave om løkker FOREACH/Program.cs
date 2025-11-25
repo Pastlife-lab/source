ba1957a10a94365ef6d35a77d9b1130ccfbe0510
Pastlife-lab <omikron36@hotmail.com>
2025-11-25 13:52:09 +0100

    a
    Foreach løkker

---
+++ repos/oppgave om løkker FOREACH/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace oppgave_om_løkker_FOREACH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = "Hei på deg!";
+            foreach (char letter in tekst)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
+//                                              Oppgave 2:
+//                      Lag en foreach-løkke som går igjennom hver bokstav i en string og printer den til konsollen.
+//                      Hint: en string kan brukes på samme måte som et array!
