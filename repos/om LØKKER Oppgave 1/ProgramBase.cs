8d720583c789ee85ac4c848fff8c62d00da5a32f
Pastlife-lab <omikron36@hotmail.com>
2025-11-25 13:29:35 +0100

    a
    Løkker oppgave 1 =)

---
+++ repos/om LØKKER Oppgave 1/ProgramBase.cs
@@ -0,0 +1,23 @@
+﻿namespace om_LØKKER_Oppgave_1
+{
+    internal class ProgramBase
+    {
+        static void Main()
+        {
+            string[] args = new string[] { "Terje" };
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{args[0]} er kul");
+            }
+        }
+    }
+}
+
+
+
+
+//
+//
+//                                          Oppgaver om løkker
+//                                            Oppgave 1:
+//                          Lag en for-løkke som printer "Terje er kul" til konsollen 5 ganger
