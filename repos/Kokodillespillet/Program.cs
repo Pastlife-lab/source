56bb9477baf582fd4a84c3f1af05a80542f91a68
Pastlife-lab <omikron36@hotmail.com>
2025-12-03 13:50:57 +0100

    Update Program.cs

--- repos/Kokodillespillet/Program.cs
+++ repos/Kokodillespillet/Program.cs
@@ -1,12 +1,56 @@
 ﻿namespace Kokodillespillet
 {
-    internal class Program
+    using System;
+
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string number = "0";
-            string input = Console.ReadLine();
-            Console.WriteLine(number);
+            Random rnd = new Random();
+            int score = 0;
+
+            while (true)
+            {               
+                int a = rnd.Next(1, 11); 
+                int b = rnd.Next(1, 11);
+
+                Console.WriteLine($"{a} _ {b}");
+
+                Console.Write("Skriv <, > eller = : ");
+                string svar = Console.ReadLine();
+
+                if (svar != "<" && svar != ">" && svar != "=")
+                {
+                    Console.WriteLine("Spillet avsluttes!");
+                    break;
+                }
+
+                string fasit;
+                if (a < b)
+                    fasit = "<";
+                else if (a > b)
+                    fasit = ">";
+                else
+                    fasit = "=";
+
+                if (svar == fasit)
+                {
+                    score++;
+                    Console.WriteLine("Riktig! +1 poeng.");
+                }
+                else
+                {
+                    score--;
+                    Console.WriteLine($"Feil! Riktig svar var {fasit}. -1 poeng.");
+                }
+
+                Console.WriteLine($"Poeng: {score}\n");
+            }
         }
     }
 }
+//                  Tallene må sjekkes om det første er større eller mindre eller lik det andre tallet,
+//                  Det må verifiseres om brukeren har valgt riktig alternativ.
+//                  Brukeren får et poeng per riktig svar, og mister et poeng per feil svar.
+//                  Poengsummen printes til skjermen for hvert svar brukeren har gitt.
+//                  Spillet avsluttes når brukeren skriver inn noe annet enn de tre alternativene.
